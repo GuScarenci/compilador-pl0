a33c4df64a4e1cf69ffd40b4de8eee3ac348657c
gabriel-libardi <libardi.120205@protonmail.com>
2024-05-07 11:47:10 -0300

    Boilerplate code for the lexical analyser

---
+++ include/lexer.h
@@ -0,0 +1,45 @@
+#ifndef __LEXER_H__
+#define __LEXER_H__
+
+#include <stdio.h>
+#include <stdint.h>
+
+/*
+ * Token Stream is an abtract data type that encapsulates
+ * a single source file and its stream of token pairs.
+ * It fulfills the task of lexical analysis, returning a stream
+ * of tokens and their types, and any lexical errors, if they occur.
+ */
+typedef FILE* TokenStream;
+
+/*
+ * A token pair is an abstract data type that encapsulates a
+ * token and its type, conformant to PL/0's grammar rules.
+ * If the token in ill-formed,the type field must point to
+ * the appropriate error code.
+ */
+typedef struct token_pair_t {
+    char* token_str;
+    uint32_t type;
+} Token;
+
+
+/*
+ * Returns the Token Stream that reads from the source
+ * file whose name is the string pointed to by 'source_path'.
+ * The tokens are analysed in conformity to PL/0's grammar.
+ */
+TokenStream* token_stream_init(const char* restrict source_path);
+
+/*
+ * Frees resources utilized by Token Stream.
+ */
+void token_stream_free(TokenStream* tok_stream);
+
+/*
+ * Returns the next token in the stream.
+ */
+Token* get_next_token(TokenStream* tok_stream);
+
+
+#endif
---
+++ src/lexer.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "lexer.h"
+#include "IO_utils.h"
+
+static const char* restrict type_map[] = {
+    "identifier", "integer literal", "comment"
+};
+
+
+TokenStream* token_stream_init(const char* restrict source_path) {
+    TokenStream* tok_stream;
+
+    XALLOC(TokenStream, tok_stream, 1);
+    OPEN_FILE(*tok_stream, source_path, "r");
+
+    return tok_stream;
+}
+
+void token_stream_free(TokenStream* tok_stream) {
+    if (tok_stream != NULL) {
+        fclose(*tok_stream);
+    }
+
+    free(tok_stream);
+}
+
+Token* get_next_token(TokenStream* tok_stream) {
+    if (tok_stream == NULL) {
+        ABORT_PROGRAM("Cannot fetch token from vacuous stream");
+    }
+
+    // TODO
+    // Implementar máquina de estados aqui
+
+    return NULL;
+}
