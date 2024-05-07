f30bd56a07b1a5469ceb2af7fbb4c7c8aba355b1
gabriel-libardi <libardi.120205@protonmail.com>
2024-05-07 11:51:45 -0300

    Test main for boilerplate lexer

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "lexer.h"
+
 int main(int argc, char** argv){
-    printf("Hello world\n");
+    TokenStream* tok_stream = token_stream_init("a.pl0");
+    token_stream_free(tok_stream);
 }
