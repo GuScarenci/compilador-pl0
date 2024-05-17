b64457e88c304464bfede8e6f0b9e586321c11cb
gabriel-libardi <libardi.120205@protonmail.com>
2024-05-17 18:09:41 -0300

    Add prototype for isLetter

--- include/str_utils.h
+++ include/str_utils.h
@@ -11,12 +11,34 @@ char* trimNewline(char* str);
 // Function to split a line into tokens by a delimiter
 char** split(const char* line, const char* delimiter, int* count);
 
+/*
+ * This function returns if a character is a numerical digit.
+ * In our mini-regex notation, this recognizes \d.
+ */
 bool isDigit(char c);
 
+/*
+ * This function returns if a character is a letter or valid symbol in PL/0.
+ * In our mini-regex notation, this recognizes \D.
+ */
 bool isNonDigit(char c);
 
+/*
+ * This function returns if a character is a valid symbol in PL/0.
+ * In our mini-regex notation, this recognizes \p.
+ */
 bool isSymbol(char c);
 
+/*
+ * This function returns if a character is a letter.
+ * In our mini-regex notation, this recognizes \l.
+ */
+bool isLetter(char c);
+
+/*
+ * This function returns if a character is a whitespace.
+ * In our mini-regex notation, this recognizes \s.
+ */
 bool isWhitespace(char c);
 
 #endif
