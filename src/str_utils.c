dabc77b9d69948dbfbbbeba00c7c0a6be7332aff
ArturWeber <88106656+ArturWeber@users.noreply.github.com>
2024-05-19 20:55:53 -0300

    Coloquei headers com créditos em todos os arquivos

--- src/str_utils.c
+++ src/str_utils.c
@@ -1,3 +1,22 @@
+/************************************************************
+*             Trabalho 1 - SCC0605                          *
+*                                                           *
+*      Nome: Artur Brenner Weber                            *
+*      nUSP: 12675451                                       *
+*      Nome: Gabriel Franceschi Libardi                     *
+*      nUSP: 11760739                                       *
+*      Nome: Guilherme Castanon Silva Pereira               *
+*      nUSP: 11801140                                       *
+*      Nome: Gustavo Scarenci de Carvalho Pereira           *
+*      nUSP: 12547792                                       *
+*      Nome: Matheus Henrique Dias Cirillo                  *
+*      nUSP: 12547750                                       *
+*      Data de última atualizacao: 19/5/2024                *
+*      Ambiente: VSCode 1.89.1                              *
+*                                                           *
+*           Arquivo str_utils.c                             *
+************************************************************/
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
