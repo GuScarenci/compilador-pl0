203aff3c1c7257b75538d84d8e274d67e4ff4537
guscarenci <gustavomourascarenci@usp.br>
2024-06-22 19:01:24 -0300

    adds header on error_handler.c

--- src/error_handler.c
+++ src/error_handler.c
@@ -1,3 +1,21 @@
+/************************************************************
+*             Trabalho 2 - SCC0605                          *
+*                                                           *
+*      Nome: Artur Brenner Weber                            *
+*      nUSP: 12675451                                       *
+*      Nome: Gabriel Franceschi Libardi                     *
+*      nUSP: 11760739                                       *
+*      Nome: Guilherme Castanon Silva Pereira               *
+*      nUSP: 11801140                                       *
+*      Nome: Gustavo Scarenci de Carvalho Pereira           *
+*      nUSP: 12547792                                       *
+*      Nome: Matheus Henrique Dias Cirillo                  *
+*      nUSP: 12547750                                       *
+*      Data de última atualizacao: 22/06/2024               *
+*      Ambiente: VSCode 1.89.1                              *
+*                                                           *
+*           Arquivo error_handler.c                         *
+************************************************************/
 #include "error_handler.h"
 
 static size_t error_count = 0;
